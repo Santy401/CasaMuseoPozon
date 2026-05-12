45ccd6a3236f313c38491af147fab494c2718ac8
Santy401 <santiprocastellar6@gmail.com>
2026-05-12 02:51:27 +0000

    feat: migrate domain to .org, add robots.txt, and update schema markup and sitemap

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -4,17 +4,23 @@ export const GET: APIRoute = () => {
   return new Response(`
     <?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url><loc>https://www.casapozonmuseo.site/</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/quienes-somos</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/modelo-museal</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/nodos-museales</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/recorrido-museal</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/proyectos-sociales</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/portafolio</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/eventos</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/representante-legal</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/socios</loc></url>
-      <url><loc>https://www.casapozonmuseo.site/pedagogia-territorial</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/quienes-somos</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/modelo-museal</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/nodos-museales</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/recorrido-museal</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/proyectos-sociales</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/portafolio</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/eventos</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/representante-legal</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/socios</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/pedagogia-territorial</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/noticias</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/galeria</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/infraestructura</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/coleccion</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/como-apoyar</loc></url>
+      <url><loc>https://www.casapozonmuseo.org/politica-privacidad</loc></url>
     </urlset>
   `.trim(), {
     headers: { 'Content-Type': 'application/xml' }
